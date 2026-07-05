7f88f7f1a0d371696efe10b72ec212240cf1ae87
Kumarsai <saikumarcme@gmail.com>
2026-07-05 10:46:30 +0530

    AI project initial commit

---
+++ AI/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using OpenAI;
+using OpenAI.Responses;
+using System.ClientModel;
+
+#pragma warning disable OPENAI001
+
+Console.WriteLine("Hello, World!");
+
+const string deploymentName = "gpt-5.4-mini";
+const string endpoint = "https://suku-ai-103-learning-resource.services.ai.azure.com/openai/v1";
+const string apiKey = "1O68BhKFEZWcXyVl2oSedqrQLxPBHmDobhKX1hg4p9FYy1iTVUbbJQQJ99CGACHYHv6XJ3w3AAAAACOGbXxd";
+ResponsesClient client = new(
+    credential: new ApiKeyCredential(apiKey),
+    options: new ResponsesClientOptions
+    {
+        Endpoint = new Uri(endpoint)
+    });
+
+CreateResponseOptions options = new()
+{
+    Model = deploymentName,
+    InputItems =
+    {
+        ResponseItem.CreateUserMessageItem(
+            "What's the weather like today for my current location?")
+    }
+};
+
+ResponseResult response = client.CreateResponse(options);
+
+Console.WriteLine($"[ASSISTANT]: {response.GetOutputText()}");
