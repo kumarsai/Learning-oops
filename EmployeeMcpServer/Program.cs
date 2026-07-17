256c07103e26e60d3a3efa3a314b1bae706104a7
saikumararrola <saikumararrola@hotmail.com>
2026-07-17 17:08:12 +0530

    Created running mcp

---
+++ EmployeeMcpServer/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+
+await builder.Build().RunAsync();
+
+[McpServerToolType]
+public static class EmployeeTools
+{
+    [McpServerTool]
+    [Description("Gets employee information by employee ID.")]
+    public static string GetEmployee(
+        [Description("Employee ID")] int employeeId)
+    {
+        return employeeId switch
+        {
+            101 => """
+                   {
+                     "id": 101,
+                     "name": "Sai",
+                     "department": "Engineering"
+                   }
+                   """,
+
+            _ => """{"error":"Employee not found"}"""
+        };
+    }
+}
